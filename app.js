a4cba08bb8b99d881fbabdd15e80a24fc5c24778
Ivan Fedorusov <ifedorusov@gmail.com>
2020-03-12 07:02:42 +0200

    To Do List

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    };
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    };
+    // Some code
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
     // Some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(5);
-
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
-    };
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        // 1. Get the filed input data
+        // 2. Add the item to the budget controller
+        // 3. Add the item to the UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UI
+    });
 })(budgetController, UIController);
