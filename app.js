351f3fb84e2bdace074e1195e9a1cce8464913de
Ivan Fedorusov <ifedorusov@gmail.com>
2020-03-12 07:41:00 +0200

    Get the filed input data

--- app.js
+++ app.js
@@ -5,16 +5,49 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-    // Some code
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                desription: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn').addEventListener('click', function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
         // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
         // 2. Add the item to the budget controller
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
+    };
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
     });
 })(budgetController, UIController);
