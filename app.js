1706d26bbb86ba83328bae405d7c8debd442e884
Ivan Fedorusov <ifedorusov@gmail.com>
2020-03-14 20:27:32 +0200

    Set EventListener to .container cuz eventDelegation

--- app.js
+++ app.js
@@ -103,7 +103,8 @@ var UIController = (function() {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     };
 
     return {
@@ -125,7 +126,7 @@ var UIController = (function() {
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
                 html =
-                    '<div class="item clearfix" id="income-%id%">\
+                    '<div class="item clearfix" id="inc-%id%">\
                     <div class="item__description">%description%</div>\
                     <div class="right clearfix"><div class="item__value">%value%</div>\
                     <div class="item__delete"><button class="item__delete--btn">\
@@ -133,7 +134,7 @@ var UIController = (function() {
             } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
                 html =
-                    '<div class="item clearfix" id="expense-%id%">\
+                    '<div class="item clearfix" id="exp-%id%">\
                     <div class="item__description">%description%</div>\
                     <div class="right clearfix"><div class="item__value">\
                     %value%</div> <div class="item__percentage">21%</div>\
@@ -207,6 +208,10 @@ var controller = (function(budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
         });
+
+        document
+            .querySelector(DOM.container)
+            .addEventListener('click', ctrlDeleteItem);
     };
 
     var updateBudget = function() {
@@ -246,6 +251,25 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+        }
+    };
+
+    var ctrlDeleteItem = function(event) {
+        var itemID, splitID, type, ID;
+
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (itemID) {
+            //inc-1
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
+
+            // 1. Delete the item from the data structure
+
+            // 2. Delete the item from the UI
+
+            // 3. Update and show the new budget
         }
     };
 
