9e6a8d6cc1af1930ccd05abd48f2b3c56fed9df5
Ivan Fedorusov <ifedorusov@gmail.com>
2020-03-21 13:37:12 +0200

    Improve input fields with toggling color depending the type

--- app.js
+++ app.js
@@ -160,7 +160,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -196,6 +197,12 @@ var UIController = (function() {
             '.' +
             dec
         );
+    };
+
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
     };
 
     return {
@@ -295,12 +302,6 @@ var UIController = (function() {
                 DOMstrings.expensesPercLabel
             );
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
                 if (percentages[index] > 0) {
                     current.textContent = percentages[index] + '%';
@@ -308,6 +309,49 @@ var UIController = (function() {
                     current.textContent = '---';
                 }
             });
+        },
+
+        displayMonth: function() {
+            var now, months, month, year;
+
+            now = new Date();
+            // var christmas = new Date(2020, 11, 25)
+
+            months = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent =
+                months[month] + ' ' + year;
+        },
+
+        changedType: function() {
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType +
+                    ',' +
+                    DOMstrings.inputDescription +
+                    ',' +
+                    DOMstrings.inputValue
+            );
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
 
         getDOMstrings: function() {
@@ -334,6 +378,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         document
             .querySelector(DOM.container)
             .addEventListener('click', ctrlDeleteItem);
+
+        document
+            .querySelector(DOM.inputType)
+            .addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function() {
@@ -418,6 +466,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
